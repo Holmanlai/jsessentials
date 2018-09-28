e61a2f9be75e4384ad24496eee0aff08c657dad3
Hal Tang <hal.wh.tang@gmail.com>
2018-09-27 17:21:16 -0700

    Implement message send feature using socket.io on client and server side

--- client/app.js
+++ client/app.js
@@ -1,5 +1,12 @@
+let socket = io();
+
 $('form').on('submit',function () {
-    var text = $('#message').val();
-    alert(text);
+    var text = $('#initials').val() + ' says: ' + $('#message').val();
+    socket.emit('message', text);
+    $('#message').val('');
     return false;
   });
+
+socket.on('message', function (msg) {
+  $('<li>').text(msg).appendTo('#history');
+});
--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 let express = require('express');
-let app = express();
-
 let http = require('http');
-let server = http.Server(app);
+let app = express();
 
 app.use(express.static('client'));
 
+let server = http.Server(app);
+let io = require('socket.io')(server);
+
+io.on('connection', function (socket) {
+  console.log('Connection Established');
+  socket.on('message', function (msg) {
+    io.emit('message', msg);
+  });
+});
 server.listen(3000, function() {
   console.log('Chat server running');
 });
